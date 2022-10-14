7060003fb51051d3e1d0c984f93601ce3c5e214e
oali-dev <omar_ali@outlook.jp>
2022-10-14 03:37:23 -0400

    Added tilemap collision support
    -Added collidable borders along edges of camera
    -Added collider to obstacle tilemap
    -Set Pixel Perfect Camera to crop X and Y

--- Flick/Assets/Scripts/System/GameManager.cs
+++ Flick/Assets/Scripts/System/GameManager.cs
@@ -11,10 +11,29 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
+        // TODO: Don't do this
         GameObject starGameObject = GameObject.Find("Star");
         StarController starController = starGameObject.GetComponent<StarController>();
+
         _inputManager = new InputManager(starController);
         _levelInstance = new LevelInstance(levelData);
+    }
+
+    // Create collidable boarders along each edge of the camera using an edge collider
+    // Need to call after WaitForEndOfFrame because Pixel Perfect Camera changes the viewport in LateUpdate
+    // TODO: Make this not bad
+    private IEnumerator Start()
+    {
+        yield return new WaitForEndOfFrame();
+        Camera camera = Camera.main;
+        Vector2 bottomLeft = camera.ViewportToWorldPoint(new Vector3(0, 0, camera.nearClipPlane));
+        Vector2 topLeft = camera.ViewportToWorldPoint(new Vector3(0, 1, camera.nearClipPlane));
+        Vector2 topRight = camera.ViewportToWorldPoint(new Vector3(1, 1, camera.nearClipPlane));
+        Vector2 bottomRight = camera.ViewportToWorldPoint(new Vector3(1, 0, camera.nearClipPlane));
+
+        Vector2[] edgeColliderPoints = new[] { bottomLeft, topLeft, topRight, bottomRight, bottomLeft };
+        List<Vector2> pointList = new List<Vector2>(edgeColliderPoints);
+        camera.gameObject.AddComponent<EdgeCollider2D>().SetPoints(pointList);
     }
 
     private void Update()
