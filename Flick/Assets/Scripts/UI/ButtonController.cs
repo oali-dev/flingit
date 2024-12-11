3c0a0bb43391bd0827ee8a1697a8b83714a9afad
oali-dev <omar_ali@outlook.jp>
2024-12-11 17:45:03 -0500

    Added animated buttons
    - Animated the scale of non-menu buttons when they become active in the scene
    - Changed the color of buttons when they are pressed down
    - Made minor adjustments to levels 2 and 9

--- Flick/Assets/Scripts/UI/ButtonController.cs
+++ Flick/Assets/Scripts/UI/ButtonController.cs
@@ -6,10 +6,45 @@ public class ButtonController : MonoBehaviour
 {
     public delegate void OnButtonPressCallback();
     public OnButtonPressCallback _onButtonPress;
-    private int _currentLevel = 1;
+
+    private int _currentLevel = 0;
+    private const float InitialScale = 0.2f;
+    private const float GrowSpeed = 2.0f;
+
+    private void Start()
+    {
+        ParseSceneName();
+    }
+
+    public void LoadMainMenu()
+    {
+        if(_onButtonPress != null)
+        {
+            _onButtonPress();
+        }
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadLevel()
+    {
+        if(_onButtonPress != null)
+        {
+            _onButtonPress();
+        }
+        SceneManager.LoadScene("Level" + _currentLevel);
+    }
+
+    public void LoadNextLevel()
+    {
+        if(_onButtonPress != null)
+        {
+            _onButtonPress();
+        }
+        SceneManager.LoadScene("Level" + (_currentLevel + 1));
+    }
 
     // Parse the number at the end of the scene name to get the current level
-    private void Start()
+    private void ParseSceneName()
     {
         string currentLevelString = SceneManager.GetActiveScene().name;
         int i = currentLevelString.Length - 1;
@@ -43,32 +78,5 @@ public class ButtonController : MonoBehaviour
         {
             _currentLevel = Int32.Parse(currentLevelString.Substring(i));
         }
-    }
-
-    public void LoadMainMenu()
-    {
-        if(_onButtonPress != null)
-        {
-            _onButtonPress();
-        }
-        SceneManager.LoadScene("MainMenu");
-    }
-
-    public void LoadLevel()
-    {
-        if(_onButtonPress != null)
-        {
-            _onButtonPress();
-        }
-        SceneManager.LoadScene("Level" + _currentLevel);
-    }
-
-    public void LoadNextLevel()
-    {
-        if(_onButtonPress != null)
-        {
-            _onButtonPress();
-        }
-        SceneManager.LoadScene("Level" + (_currentLevel + 1));
     }
 }
